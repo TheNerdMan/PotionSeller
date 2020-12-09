6c2e845463629522755bff0014b6e6e8b3c9aeea
Alex Wilson <alex.dave999@gmail.com>
2020-12-09 12:33:37 +0000

    Init (ping/pong)

---
+++ PotionSeller.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * A ping pong bot, whenever you send "ping", it replies "pong".
+ */
+
+// Import the discord.js module
+const Discord = require('discord.js');
+
+// Create an instance of a Discord client
+const client = new Discord.Client();
+const botToken = 'Nzg2MjA2ODQ3NzUyNDcwNTQ4.X9DCLw.EaWx27qxd4IexNH2RRNiAiLU8KQ';
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "ping"
+  if (message.content === 'ping') {
+    // Send "pong" to the same channel
+    message.channel.send('pong');
+  }
+});
+
+// Log our bot in using the token from https://discord.com/developers/applications
+client.login(botToken);
